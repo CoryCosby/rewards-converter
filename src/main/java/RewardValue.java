cc6e26d93bbd1ea2d44fdf5066bc8540535c066b
Cortayshious Cosby <taycosby25@gmail.com>
2024-01-29 11:19:44 -0600

    updated business logic in RewardValue class. eliminated the need to store miles variable.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,26 +1,28 @@
 public class RewardValue {
     private final double cashValue;
-    private int milesValue;
-    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.035;
-    
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
-        
     }
 
     public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
-        this.cashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
-        return this.cashValue; 
+        return cashValue;
     }
 
     public int getMilesValue() {
-        return this.milesValue;
+        return convertToMiles(this.cashValue);
     }
-    
 }
